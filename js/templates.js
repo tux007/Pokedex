a801b06f86617831bf5b2d77d181f6fbebf642be
Thomas Steimann <thomas@steimann.ch>
2025-08-11 19:55:49 +0200

    Zusätzliche Info auf der Detailkarte hinzugefügt

--- js/templates.js
+++ js/templates.js
@@ -17,10 +17,17 @@ function createPokemonCardTemplate(poke, typeColors) {
 }
 
 function createModalTemplate(poke) {
+  const abilities = poke.abilities.map(ability => ability.ability.name).join(', ');
+  const heightInMeters = (poke.height / 10).toFixed(1);
+  const weightInKg = (poke.weight / 10).toFixed(1);
+  
   return `
     <div class="close-modal" onclick="closeModal()">×</div>
     <h2>${poke.name.toUpperCase()}</h2>
     <img src="${poke.sprites.front_default}" alt="${poke.name}">
+    <p><strong>Height:</strong> ${heightInMeters} m</p>
+    <p><strong>Weight:</strong> ${weightInKg} kg</p>
+    <p><strong>Abilities:</strong> ${abilities}</p>
     <p><strong>HP:</strong> ${poke.stats[0].base_stat}</p>
     <p><strong>Attack:</strong> ${poke.stats[1].base_stat}</p>
     <p><strong>Defense:</strong> ${poke.stats[2].base_stat}</p>
